a15fead3e31f6c20560700cb92f6ccc047d4b70d
Hardik Nagpal <66942380+hardik1008@users.noreply.github.com>
2022-04-24 16:55:08 +0530

    Add files via upload

---
+++ Assets/JS/habits_app.js
@@ -0,0 +1,25 @@
+
+// function handleClickEvent(e)
+// {
+//     const target=e.target;
+//     // console.log(target);
+//     if(target.classList.contains('fa-star'))
+//     {
+//         if(target.classList.contains('far')){
+            
+//             target.classList.remove('far');
+//             target.classList.add('fas');
+
+//         }
+//         else
+//         {
+//             target.classList.remove('fas');
+//             target.classList.add('far');
+//         }
+//     }
+
+    
+
+// }
+
+// document.addEventListener('click',handleClickEvent)
